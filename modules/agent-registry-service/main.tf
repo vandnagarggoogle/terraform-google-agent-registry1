34da277ecf02ebb6ec2848a900f7eef3c1d349cc
Vandna Garg <vandnagarg@google.com>
2026-03-31 09:59:52 +0000

    add project

--- modules/agent-registry-service/main.tf
+++ modules/agent-registry-service/main.tf
@@ -18,7 +18,7 @@ resource "google_agent_registry_service" "this" {
   provider   = google-nightly
   location   = var.location
   service_id = var.service_id
-  project = project_id
+  project = var.project_id
   display_name = var.display_name
   description  = var.description
 
