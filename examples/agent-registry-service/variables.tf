532dd749c7bd25a14508348a13011c749d2873ad
Vandna Garg <vandnagarg@google.com>
2026-04-02 07:22:32 +0000

    remove random

--- examples/agent-registry-service/variables.tf
+++ examples/agent-registry-service/variables.tf
@@ -18,9 +18,3 @@ variable "project_id" {
   description = "The GCP project ID where the Agent Registry resources will be tested."
   type        = string
 }
-
-variable "random_string" {
-  description = "A random string used to ensure resource names are unique across test runs."
-  type        = string
-  default     = "test"
-}
