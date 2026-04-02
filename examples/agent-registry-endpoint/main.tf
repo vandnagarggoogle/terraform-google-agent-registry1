0e159ecc54cbcf1380538d4da555a3930b5b8433
Vandna Garg <vandnagarg@google.com>
2026-04-02 07:04:47 +0000

    adding endpoint

--- examples/agent-registry-endpoint/main.tf
+++ examples/agent-registry-endpoint/main.tf
@@ -14,21 +14,11 @@
  * limitations under the License.
  */
 
-module "service_prereq" {
-  source     = "../../modules/agent-registry-service"
-  project_id = var.project_id
-  location   = "us-central1"
-  service_id = "test-endpoint-abc"
-  endpoint_spec = {
-    type = "NO_SPEC"
-  }
-}
-
 module "endpoint_data" {
   source      = "../../modules/agent-registry-endpoint"
   project_id  = var.project_id
   location    = "us-central1"
-  endpoint_id = module.service_prereq.service_id
+  endpoint_id = "endpoint-id"
 }
 
 output "endpoint_uri" { value = module.endpoint_data.endpoint_id }
